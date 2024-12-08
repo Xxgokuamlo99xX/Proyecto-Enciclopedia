ea61b08017e8315841db06a40e7e955351e2fd80
Ethianne2237 <ethiannepro@gmail.com>
2024-12-08 13:44:41 -0700

    k3

---
+++ Nomal/Menu_inicio.Designer.vb
@@ -0,0 +1,229 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Menu_inicio
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Menu_inicio))
+        Me.IconButton3 = New FontAwesome.Sharp.IconButton()
+        Me.IconButton1 = New FontAwesome.Sharp.IconButton()
+        Me.IconButton2 = New FontAwesome.Sharp.IconButton()
+        Me.IconButton4 = New FontAwesome.Sharp.IconButton()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PanelDesktop = New System.Windows.Forms.Panel()
+        Me.MashDataSet1 = New Proyecto_Enciclopedia.mashDataSet()
+        Me.Panelmenu = New Siticone.Desktop.UI.Winforms.SiticoneGradientPanel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel2.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelDesktop.SuspendLayout()
+        CType(Me.MashDataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panelmenu.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'IconButton3
+        '
+        Me.IconButton3.BackColor = System.Drawing.Color.Transparent
+        Me.IconButton3.Dock = System.Windows.Forms.DockStyle.Top
+        Me.IconButton3.FlatAppearance.BorderSize = 0
+        Me.IconButton3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red
+        Me.IconButton3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Me.IconButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.IconButton3.Font = New System.Drawing.Font("Fixedsys Excelsior 3.01", 20.0!)
+        Me.IconButton3.ForeColor = System.Drawing.Color.White
+        Me.IconButton3.IconChar = FontAwesome.Sharp.IconChar.PersonWalkingArrowRight
+        Me.IconButton3.IconColor = System.Drawing.Color.White
+        Me.IconButton3.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButton3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.IconButton3.Location = New System.Drawing.Point(0, 460)
+        Me.IconButton3.Name = "IconButton3"
+        Me.IconButton3.Size = New System.Drawing.Size(204, 101)
+        Me.IconButton3.TabIndex = 3
+        Me.IconButton3.Text = "Salir"
+        Me.IconButton3.UseVisualStyleBackColor = False
+        '
+        'IconButton1
+        '
+        Me.IconButton1.BackColor = System.Drawing.Color.Transparent
+        Me.IconButton1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.IconButton1.FlatAppearance.BorderSize = 0
+        Me.IconButton1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Yellow
+        Me.IconButton1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Me.IconButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.IconButton1.Font = New System.Drawing.Font("Fixedsys Excelsior 3.01", 12.0!)
+        Me.IconButton1.ForeColor = System.Drawing.Color.White
+        Me.IconButton1.IconChar = FontAwesome.Sharp.IconChar.Pencil
+        Me.IconButton1.IconColor = System.Drawing.Color.White
+        Me.IconButton1.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButton1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.IconButton1.Location = New System.Drawing.Point(0, 359)
+        Me.IconButton1.Name = "IconButton1"
+        Me.IconButton1.Size = New System.Drawing.Size(204, 101)
+        Me.IconButton1.TabIndex = 1
+        Me.IconButton1.Text = "Administrador"
+        Me.IconButton1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.IconButton1.UseVisualStyleBackColor = False
+        '
+        'IconButton2
+        '
+        Me.IconButton2.BackColor = System.Drawing.Color.Transparent
+        Me.IconButton2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.IconButton2.FlatAppearance.BorderSize = 0
+        Me.IconButton2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime
+        Me.IconButton2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Me.IconButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.IconButton2.Font = New System.Drawing.Font("Fixedsys Excelsior 3.01", 20.0!)
+        Me.IconButton2.ForeColor = System.Drawing.Color.White
+        Me.IconButton2.IconChar = FontAwesome.Sharp.IconChar.PeopleGroup
+        Me.IconButton2.IconColor = System.Drawing.Color.White
+        Me.IconButton2.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButton2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.IconButton2.Location = New System.Drawing.Point(0, 258)
+        Me.IconButton2.Name = "IconButton2"
+        Me.IconButton2.Size = New System.Drawing.Size(204, 101)
+        Me.IconButton2.TabIndex = 2
+        Me.IconButton2.Text = "Usuario"
+        Me.IconButton2.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.IconButton2.UseVisualStyleBackColor = False
+        '
+        'IconButton4
+        '
+        Me.IconButton4.AutoSize = True
+        Me.IconButton4.BackColor = System.Drawing.Color.Transparent
+        Me.IconButton4.Dock = System.Windows.Forms.DockStyle.Top
+        Me.IconButton4.FlatAppearance.BorderSize = 0
+        Me.IconButton4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Purple
+        Me.IconButton4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Me.IconButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.IconButton4.Font = New System.Drawing.Font("Fixedsys Excelsior 3.01", 20.0!)
+        Me.IconButton4.ForeColor = System.Drawing.Color.White
+        Me.IconButton4.IconChar = FontAwesome.Sharp.IconChar.House
+        Me.IconButton4.IconColor = System.Drawing.Color.White
+        Me.IconButton4.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButton4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.IconButton4.Location = New System.Drawing.Point(0, 157)
+        Me.IconButton4.Name = "IconButton4"
+        Me.IconButton4.Size = New System.Drawing.Size(204, 101)
+        Me.IconButton4.TabIndex = 4
+        Me.IconButton4.Text = "Inicio"
+        Me.IconButton4.UseVisualStyleBackColor = False
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.Transparent
+        Me.Panel2.Controls.Add(Me.PictureBox2)
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel2.Location = New System.Drawing.Point(0, 0)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(204, 157)
+        Me.Panel2.TabIndex = 0
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(204, 157)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PanelDesktop
+        '
+        Me.PanelDesktop.AutoSize = True
+        Me.PanelDesktop.BackColor = System.Drawing.Color.Navy
+        Me.PanelDesktop.Controls.Add(Me.PictureBox1)
+        Me.PanelDesktop.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PanelDesktop.Location = New System.Drawing.Point(0, 0)
+        Me.PanelDesktop.Name = "PanelDesktop"
+        Me.PanelDesktop.Size = New System.Drawing.Size(1141, 586)
+        Me.PanelDesktop.TabIndex = 1
+        '
+        'MashDataSet1
+        '
+        Me.MashDataSet1.DataSetName = "mashDataSet"
+        Me.MashDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'Panelmenu
+        '
+        Me.Panelmenu.Controls.Add(Me.IconButton3)
+        Me.Panelmenu.Controls.Add(Me.IconButton1)
+        Me.Panelmenu.Controls.Add(Me.IconButton2)
+        Me.Panelmenu.Controls.Add(Me.IconButton4)
+        Me.Panelmenu.Controls.Add(Me.Panel2)
+        Me.Panelmenu.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panelmenu.FillColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Panelmenu.FillColor2 = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Panelmenu.GradientMode = System.Drawing.Drawing2D.LinearGradientMode.Vertical
+        Me.Panelmenu.Location = New System.Drawing.Point(0, 0)
+        Me.Panelmenu.Name = "Panelmenu"
+        Me.Panelmenu.Size = New System.Drawing.Size(204, 586)
+        Me.Panelmenu.TabIndex = 2
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(1141, 586)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Menu_inicio
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(1141, 586)
+        Me.Controls.Add(Me.Panelmenu)
+        Me.Controls.Add(Me.PanelDesktop)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Menu_inicio"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Enciclopedia"
+        Me.Panel2.ResumeLayout(False)
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelDesktop.ResumeLayout(False)
+        CType(Me.MashDataSet1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panelmenu.ResumeLayout(False)
+        Me.Panelmenu.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents IconButton1 As FontAwesome.Sharp.IconButton
+    Friend WithEvents IconButton3 As FontAwesome.Sharp.IconButton
+    Friend WithEvents IconButton2 As FontAwesome.Sharp.IconButton
+    Friend WithEvents PanelDesktop As Panel
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents IconButton4 As FontAwesome.Sharp.IconButton
+    Friend WithEvents MashDataSet1 As mashDataSet
+    Friend WithEvents Panelmenu As Siticone.Desktop.UI.Winforms.SiticoneGradientPanel
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ Nomal/Menu_inicio.vb
@@ -0,0 +1,127 @@
+﻿
+Imports FontAwesome.Sharp
+Imports System.IO
+Imports WMPLib
+Imports System.Data.SqlClient
+
+
+Public Class Menu_inicio
+    Private currentBtn As IconButton
+    Private leftborderBtn As Panel
+    Private currentchildform As Form
+
+
+    Public Sub New()
+
+        ' Esta llamada es exigida por el diseñador.
+        InitializeComponent()
+
+        ' Agregue cualquier inicialización después de la llamada a InitializeComponent().
+        leftborderBtn = New Panel()
+        leftborderBtn.Size = New Size(7, 101)
+        Panelmenu.Controls.Add(leftborderBtn)
+
+    End Sub
+
+    Private Sub ActivateButton(senderBtn As Object, customColor As Color)
+        If senderBtn IsNot Nothing Then
+            DisableButton()
+            ' Button
+            currentBtn = CType(senderBtn, IconButton)
+            currentBtn.BackColor = Color.Transparent
+            currentBtn.ForeColor = customColor
+            currentBtn.IconColor = customColor
+            currentBtn.TextAlign = ContentAlignment.MiddleRight
+            currentBtn.ImageAlign = ContentAlignment.MiddleLeft
+            currentBtn.TextImageRelation = TextImageRelation.TextBeforeImage
+
+            ' Left Border
+            leftborderBtn.BackColor = customColor
+            leftborderBtn.Location = New Point(0, currentBtn.Location.Y)
+            leftborderBtn.Visible = True
+            leftborderBtn.BringToFront()
+
+
+        End If
+    End Sub
+    Private Sub OpenChildForm(childForm As Form)
+        ' Open only form
+        If currentchildform IsNot Nothing Then
+            currentchildform.Close()
+        End If
+        currentchildform = childForm
+        ' End
+
+        childForm.TopLevel = False
+        childForm.FormBorderStyle = FormBorderStyle.None
+        childForm.Dock = DockStyle.Fill
+        PanelDesktop.Controls.Add(childForm)
+        PanelDesktop.Tag = childForm
+        childForm.BringToFront()
+        childForm.Show()
+    End Sub
+
+    Private Sub DisableButton()
+        If currentBtn IsNot Nothing Then
+            currentBtn.ForeColor = Color.White
+            currentBtn.IconColor = Color.White
+            currentBtn.TextAlign = ContentAlignment.MiddleRight
+            currentBtn.ImageAlign = ContentAlignment.MiddleLeft
+            currentBtn.TextImageRelation = TextImageRelation.ImageBeforeText
+        End If
+    End Sub
+
+
+    Private Sub usuario(sender As Object, e As EventArgs) Handles IconButton2.Click
+        ActivateButton(sender, Color.Lime)
+        If currentchildform IsNot Nothing Then
+            currentchildform.Close()
+        End If
+        OpenChildForm(New Usuario)
+        Inicio_sesion.Hide()
+    End Sub
+
+    Private Sub admin(sender As Object, e As EventArgs) Handles IconButton1.Click
+        ActivateButton(sender, Color.Yellow)
+        If currentchildform IsNot Nothing Then
+            currentchildform.Close()
+        End If
+        Inicio_sesion.Show()
+    End Sub
+
+    Private Sub salir(sender As Object, e As EventArgs) Handles IconButton3.Click
+        ActivateButton(sender, Color.Red)
+        If currentchildform IsNot Nothing Then
+            currentchildform.Close()
+        End If
+        Alert_box.Alertar(
+        "Estas seguro que quieres salir?",
+        Sub()
+            Me.Close()
+        End Sub
+        )
+        Inicio_sesion.Hide()
+    End Sub
+
+    Private Sub Menu_inicio_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ActivateButton(IconButton4, Color.Purple)
+        Pantalla_de_carga.Show()
+        'My.Computer.Audio.Play("E:\Oscar Alvarado 510\Visual Studio\Proyecto Enciclopedia\bin\Debug\Syn-Cole-Feel-Good-_NCS-Release_.wav")
+
+    End Sub
+
+    Private Sub inicio(sender As Object, e As EventArgs) Handles IconButton4.Click
+        ActivateButton(sender, Color.Purple)
+        If currentchildform IsNot Nothing Then
+            currentchildform.Close()
+        End If
+        Inicio_sesion.Hide()
+    End Sub
+
+
+    Public Sub abrir_admin()
+        OpenChildForm(New Administrado_1)
+
+    End Sub
+
+End Class
